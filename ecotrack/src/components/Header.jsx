29791a5c6eb445834ac86461b4c48dc00c259f55
Raj Singh <rajsinghofficial116@gmail.com>
2026-01-12 17:47:51 +0530

    Add ecotrack project files

---
+++ ecotrack/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = ({ title }) => {
+  return (
+    <header style={{ padding: "0.5rem", backgroundColor: "orange" }}>
+      <h1>{title}</h1>
+    </header>
+  );
+};
+
+export default Header;
