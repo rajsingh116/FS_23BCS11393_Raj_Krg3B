45a51d82ebac2c1c02b60850016693294b98bca4
rajsingh116 <149702946+rajsingh116@users.noreply.github.com>
2026-01-12 17:52:32 +0530

    Delete ecotrack directory

--- ecotrack/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = ({ title }) => {
-  return (
-    <header style={{ padding: "0.5rem", backgroundColor: "orange" }}>
-      <h1>{title}</h1>
-    </header>
-  );
-};
-
-export default Header;
